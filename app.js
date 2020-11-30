794db995a51388afa221aa853a21f99a28912645
Uranka Bazarsad <urankab@gmail.com>
2020-11-30 01:01:51 -0700

    CRUD functionality, connected to MongoDB

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express')
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv/config');
 
+
+// MIDDLEWARES
+app.use(cors());
+app.use(bodyParser.json());
+
+//IMPORT ROUTES
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute);
 
 //ROUTES
 app.get('/', (req, res) => {
-    res.send('We are home!')
+    res.send('We are home!');
 });
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts!')
-});
+//CONNECT TO DB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true }, 
+    () => console.log('Connected to MongoDB')
+);
 
 
 //LISTEN TO SERVER
---
+++ routes/posts.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// GET ALL POSTS
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+//SUBMITS A POST
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    console.log(req.body)
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// GET SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// UPDATES POST
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId },
+            { $set: { title: req.body.title } }
+        );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// DELETES POSTS
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+module.exports = router;
